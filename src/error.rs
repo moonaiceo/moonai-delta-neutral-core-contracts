b15825d39e16a25649608c6c8784026dd0edf6ed
Adil <meirambaiuliadil20020@gmail.com>
2023-05-03 23:56:49 +0600

    init commit

---
+++ src/error.rs
@@ -0,0 +1,54 @@
+use cosmwasm_std::StdError;
+use thiserror::Error;
+
+#[derive(Error, Debug, PartialEq)]
+pub enum ContractError {
+
+    #[error("Custom Error val: {val:?}")]
+    CustomError { val: String },
+
+    #[error("Failure response from submsg: {0}")]
+    SubMsgFailure(String),
+
+    #[error("Unrecognised reply id: {0}")]
+    UnrecognisedReply(u64),
+
+    #[error("Invalid reply from sub-message {id}, {err}")]
+    ReplyParseFailure { id: u64, err: String },
+
+    #[error("{0}")]
+    Std(#[from] StdError),
+
+    #[error("Unauthorized")]
+    Unauthorized {},
+
+    #[error("Cannot set to own account")]
+    CannotSetOwnAccount {},
+
+    #[error("Invalid zero amount")]
+    InvalidZeroAmount {},
+
+    #[error("Allowance is expired")]
+    Expired {},
+
+    #[error("No allowance for this account")]
+    NoAllowance {},
+
+    #[error("Minting cannot exceed the cap")]
+    CannotExceedCap {},
+
+    #[error("Logo binary data exceeds 5KB limit")]
+    LogoTooBig {},
+
+    #[error("Invalid xml preamble for SVG")]
+    InvalidXmlPreamble {},
+
+    #[error("Invalid png header")]
+    InvalidPngHeader {},
+
+    #[error("Invalid expiration value")]
+    InvalidExpiration {},
+
+    #[error("Duplicate initial balance addresses")]
+    DuplicateInitialBalanceAddresses {},
+}
